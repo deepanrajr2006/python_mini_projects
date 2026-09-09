f27a193a4e2da505373a5fc52f520e3eeadc3ed4
deepanrajr2006 <deepanrajrajesh14@gmail.com>
2026-09-09 20:07:40 +0530

    Update randomNumberGame.py

--- randomNumberGame.py
+++ randomNumberGame.py
@@ -5,6 +5,7 @@ for i in range(3):
     choice=int(input("Enter your number(1 to 10):"))
     if choice==guess:
         print("Congratulation you won the game")
+        break
     else:
         count -=1
         print(f"you lose the game,And you have {count} attempts only")

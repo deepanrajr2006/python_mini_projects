290b8ef9aba8342380bd1250a9b45c9e502eb625
deepanrajr2006 <deepanrajrajesh14@gmail.com>
2026-09-09 20:01:08 +0530

    Add files via upload

---
+++ randomNumberGame.py
@@ -0,0 +1,12 @@
+import random
+count=3
+for i in range(3):
+    guess=random.randint(1,10)
+    choice=int(input("Enter your number(1 to 10):"))
+    if choice==guess:
+        print("Congratulation you won the game")
+    else:
+        count -=1
+        print(f"you lose the game,And you have {count} attempts only")
+
+print("Better luck next time")
